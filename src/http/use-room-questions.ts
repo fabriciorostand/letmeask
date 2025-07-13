6f60bbce1163f778dde205874153965ae719881c
Fabricio Rostand Morais <fabriciorostand2000@gmail.com>
2025-07-13 00:50:40 -0300

    feat: add QuestionList component and integrate with room page

--- src/http/use-room-questions.ts
+++ src/http/use-room-questions.ts
@@ -1,14 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
-import type { GetRoomsResponse } from './types/get-rooms-response';
+import type { GetRoomsQuestionsResponse } from './types/get-room-questions-response';
 
-export function useRoomsQuestions(roomId: string) {
+export function useRoomQuestions(roomId: string) {
   return useQuery({
     queryKey: ['get-questions', roomId],
     queryFn: async () => {
       const response = await fetch(
         `http://localhost:3333/rooms/${roomId}/questions`
       );
-      const result: GetRoomsResponse = await response.json();
+      const result: GetRoomsQuestionsResponse = await response.json();
 
       return result;
     },
